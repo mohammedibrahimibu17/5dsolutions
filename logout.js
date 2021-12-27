efe3c237c9e48a72578b9ae77e24f98d0db187fb
Ibrahim Munavvar <mohammedibrahimibu17ab@gmail.com>
2021-12-28 01:45:28 +0530

    Create logout.js

---
+++ logout.js
@@ -0,0 +1,8 @@
+//logout user
+ app.get('/api/logout',auth,function(req,res){
+        req.user.deleteToken(req.token,(err,user)=>{
+            if(err) return res.status(400).send(err);
+            res.sendStatus(200);
+        });
+
+    }); 
