7717f206818e972ff7c008ebe9ff84047bc28c42
Ibrahim Munavvar <mohammedibrahimibu17ab@gmail.com>
2021-12-28 01:46:46 +0530

    Create userschema.js

---
+++ userschema.js
@@ -0,0 +1,42 @@
+var mongoose=require('mongoose');
+
+const userSchema=mongoose.Schema({
+    firstname:{
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    lastname:{
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: 1
+    },
+    password:{
+        type:String,
+        required: true,
+        minlength:8
+    },
+    password2:{
+        type:String,
+        required: true,
+        minlength:8
+
+    },
+    city:{
+        type:String,
+        required: true,
+        minlength:2
+
+    },
+    token:{
+        type: String
+    }
+});
+
+module.exports=mongoose.model('User',userSchema);
